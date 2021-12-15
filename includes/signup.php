4812d3faa8dab9c9d5cd69c981a4cba171e3df38
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2021-12-15 19:25:21 +0100

    Added grabbing data from signup form

---
+++ includes/signup.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST['submit']))
+{   
+    //Here we grab data from the form
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $sex = $_POST['sex'];
+
+    //Here we make instance of Signup controller class and
+
+    //Here we run error checking
+
+    //Here we go back to index page
+}
